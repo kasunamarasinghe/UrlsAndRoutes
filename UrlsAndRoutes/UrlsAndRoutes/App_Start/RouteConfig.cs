9324b17b8216336028c351c85f993c3656f9b1d7
kasunamarasinghe <kasun@davton.com>
2015-09-25 15:03:28 +0530

    Alised a controller and an action

--- UrlsAndRoutes/UrlsAndRoutes/App_Start/RouteConfig.cs
+++ UrlsAndRoutes/UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -11,6 +11,8 @@ namespace UrlsAndRoutes
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.MapRoute("ShopSchema2", "Shop/OldAction",new { controller = "Home", action = "Index" });
+
             routes.MapRoute("ShopSchema", "Shop/{action}",new { controller = "Home" });
 
             routes.MapRoute("", "X{controller}/{action}");
