5da17a2a6a12d3f2c7aa21c292604f5e0ca2241d
kasunamarasinghe <kasun@davton.com>
2015-09-25 14:56:24 +0530

    Mixed static URLs and default values

--- UrlsAndRoutes/UrlsAndRoutes/App_Start/RouteConfig.cs
+++ UrlsAndRoutes/UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -11,17 +11,19 @@ namespace UrlsAndRoutes
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.MapRoute("ShopSchema", "Shop/{action}",new { controller = "Home" });
+
+            routes.MapRoute("", "X{controller}/{action}");
+
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            routes.MapRoute(
-            name: "Default",
-            url: "{controller}/{action}/{id}",
-            defaults: new
+            routes.MapRoute( name: "Default", url: "{controller}/{action}/{id}", defaults: new
             {
                 controller = "Home",
                 action = "Index",
                 id = UrlParameter.Optional
             }
             );
+            routes.MapRoute("", "Public/{controller}/{action}",new { controller = "Home", action = "Index" });
         }
     }
 }
